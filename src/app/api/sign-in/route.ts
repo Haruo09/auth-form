33c06456e70779812fb625fe91247ce986505ea7
Haruo09 <fabio.hn01@gmail.com>
2025-01-29 20:49:41 -0300

    API to mysql implemented

---
+++ src/app/api/sign-in/route.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { NextResponse, NextRequest } from "next/server";
+
+import { UserData } from "@/lib/UserSchema";
+import { mysql } from "@/database/mysql";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  
+  const email = searchParams.get('email');
+  const password = searchParams.get('password');
+
+  const user: UserData[] = await mysql.query(
+    "SELECT name, email, password FROM user " +
+    "WHERE user.email = ? AND user.password = ?;",
+    [email, password]
+  );
+
+  console.log(user);
+  if (user.length == 1)
+    return NextResponse.json(user[0]);
+
+  return NextResponse.json({})
+}
+
+// export async function POST(request: Request) {
+//   const data = await request.json();
+
+//   // console.log(data);
+  
+//   return new Response(JSON.stringify(data));
+// }
